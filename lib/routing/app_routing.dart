0c72088f1e5cb0db57175ef38502373f264972c5
TatianaChF <tatiana.chefox@gmail.com>
2025-02-03 15:45:44 +0300

    moving the router to a separate file

---
+++ lib/routing/app_routing.dart
@@ -0,0 +1,33 @@
+import 'package:go_router/go_router.dart';
+import 'package:sensors/diagram_page.dart';
+import 'package:sensors/map_screen.dart';
+import 'package:sensors/my_navigation_bar.dart';
+
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    // BottomNavigationBar
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) =>
+          MyNavigationBar(navigationShell: navigationShell),
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/map',
+              builder: (context, state) => const MapScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/diagram',
+              builder: (context, state) => const DiagramPage(),
+            ),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
