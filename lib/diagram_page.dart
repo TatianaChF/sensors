cf6f21390bcd9bb95a4243f9e8c4e5e423788447
TatianaChF <tatiana.chefox@gmail.com>
2025-02-03 10:13:13 +0300

    create diagram page

---
+++ lib/diagram_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DiagramPage extends StatefulWidget {
+  const DiagramPage({
+    super.key
+  });
+
+  @override
+  State<DiagramPage> createState() => _DiagramPage();
+}
+
+class _DiagramPage extends State<DiagramPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Страница диаграмм")
+    );
+  }
+}
