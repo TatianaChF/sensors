763ab497c436c9c9f694d632232b211a868c514c
bromles <all.bromles@gmail.com>
2025-02-04 11:23:44 +0300

    change color navbar

--- lib/my_navigation_bar.dart
+++ lib/my_navigation_bar.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class MyNavigationBar extends StatelessWidget  {
+class MyNavigationBar extends StatelessWidget {
   const MyNavigationBar({
-    super.key, 
+    super.key,
     required this.navigationShell
   });
 
@@ -12,28 +12,36 @@ class MyNavigationBar extends StatelessWidget  {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: navigationShell,
-        bottomNavigationBar: BottomNavigationBar(
-          items: _buildBottomNavBarItems,
-          currentIndex: navigationShell.currentIndex,
-          onTap: (index) => navigationShell.goBranch(
-            index,
-            initialLocation: index == navigationShell.currentIndex,
+      body: navigationShell,
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+        indicatorColor: Color(0xFFD4E4D7),
+        labelTextStyle: WidgetStateProperty.all(
+        const TextStyle(color: Colors.white,)
+      )),
+      child: NavigationBar(
+        backgroundColor: Color(0xFF102C14),
+        animationDuration: const Duration(seconds: 1),
+        selectedIndex: navigationShell.currentIndex,
+        destinations: const [
+          NavigationDestination(
+              selectedIcon: Icon(Icons.location_on),
+              icon: Icon(Icons.location_on_outlined, color: Colors.white),
+              label: "Карта"
           ),
-        ),
-    );
+          NavigationDestination(
+            selectedIcon: Icon(Icons.inbox),
+            icon: Icon(Icons.inbox_outlined, color: Colors.white),
+            label: "Диаграммы",
+          ),
+        ],
+        onDestinationSelected: (index) =>
+            navigationShell.goBranch(
+              index,
+              initialLocation: index == navigationShell.currentIndex,
+            ),
+      ),
+    ));
   }
-
-  List<BottomNavigationBarItem> get _buildBottomNavBarItems => [
-    const BottomNavigationBarItem(
-        icon: Icon(Icons.location_on_outlined),
-        label: "Карта"
-    ),
-    const BottomNavigationBarItem(
-        icon: Icon(Icons.inbox_outlined),
-        label: "Диаграммы",
-    ),
-  ];
 }
-
 
