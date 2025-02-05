cee66d8801e16cb134fcb61e8a33f99c0a3d64a6
bromles <all.bromles@gmail.com>
2025-02-05 11:48:27 +0300

    creating a chart container

--- lib/pages/diagram_page.dart
+++ lib/pages/diagram_page.dart
@@ -16,7 +16,17 @@ class _DiagramPage extends State<DiagramPage> {
     return Scaffold(
       body: Center(
         child: Container(
-          child: Text("Страница диаграмм")
+          width: 800,
+          height: 100,
+          padding: const EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            color: Color(0xFF102C14),
+            borderRadius: BorderRadius.circular(20.0)
+          ),
+          child: Card(
+            color: Color(0xFFD4E4D7),
+            child: Text("Страница диаграмм"),
+          )
         )
       )
     );
