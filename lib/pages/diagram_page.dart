a17f4415726d30c2107c3e375123c93d8ddefaab
bromles <all.bromles@gmail.com>
2025-02-06 12:09:04 +0300

    display sensors from the list

--- lib/pages/diagram_page.dart
+++ lib/pages/diagram_page.dart
@@ -32,45 +32,48 @@ class _DiagramPage extends State<DiagramPage> {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: Row(
-        children: [
-          Container(
-            width: 800,
-            height: 100,
-            padding: const EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              color: Color(0xFF102C14),
-              borderRadius: BorderRadius.circular(20.0)
-            ),
-            margin: EdgeInsets.symmetric(horizontal: width * 0.03, vertical: height * 0.05),
-            child: Card(
-              color: Color(0xFFD4E4D7),
-              child: Container(
-                margin: EdgeInsets.only(left: 15, right: 15),
-                child: Row(
-                  children: [
-                    Text(
-                        "Диаграмма 1",
-                        style: TextStyle(
-                            color: Color(0xFF102C14),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20
-                        )
-                    ),
-                    Spacer(),
-                    Icon(
-                      Icons.arrow_drop_down,
+    return ListView.builder(
+      itemCount: sensorData.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Row(
+            children: [
+              Container(
+                  width: 800,
+                  height: 100,
+                  padding: const EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
                       color: Color(0xFF102C14),
-                      size: 35,
-                    )
-                  ],
-                )
+                      borderRadius: BorderRadius.circular(20.0)
+                  ),
+                  margin: EdgeInsets.symmetric(horizontal: width * 0.03, vertical: height * 0.05),
+                  child: Card(
+                      color: Color(0xFFD4E4D7),
+                      child: Container(
+                          margin: EdgeInsets.only(left: 15, right: 15),
+                          child: Row(
+                            children: [
+                              Text(
+                                  "${sensorData[index].name}",
+                                  style: TextStyle(
+                                      color: Color(0xFF102C14),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20
+                                  )
+                              ),
+                              Spacer(),
+                              Icon(
+                                Icons.arrow_drop_down,
+                                color: Color(0xFF102C14),
+                                size: 35,
+                              )
+                            ],
+                          )
+                      )
+                  )
               )
-            )
-          )
-        ]
-      )
+            ]
+        );
+      }
     );
   }
 }
