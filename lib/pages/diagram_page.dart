3c069eba2aab92318d585e09d8fbcfcce65436b3
bromles <all.bromles@gmail.com>
2025-02-06 13:12:51 +0300

    moving the card code for the diagram to a separate file

--- lib/pages/diagram_page.dart
+++ lib/pages/diagram_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sensors/widgets/diagram_card.dart';
 
 class DiagramData {
   String? name;
@@ -35,44 +36,7 @@ class _DiagramPage extends State<DiagramPage> {
     return ListView.builder(
       itemCount: sensorData.length,
       itemBuilder: (BuildContext context, int index) {
-        return Row(
-            children: [
-              Container(
-                  width: 800,
-                  height: 100,
-                  padding: const EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                      color: Color(0xFF102C14),
-                      borderRadius: BorderRadius.circular(20.0)
-                  ),
-                  margin: EdgeInsets.symmetric(horizontal: width * 0.03, vertical: height * 0.05),
-                  child: Card(
-                      color: Color(0xFFD4E4D7),
-                      child: Container(
-                          margin: EdgeInsets.only(left: 15, right: 15),
-                          child: Row(
-                            children: [
-                              Text(
-                                  "${sensorData[index].name}",
-                                  style: TextStyle(
-                                      color: Color(0xFF102C14),
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20
-                                  )
-                              ),
-                              Spacer(),
-                              Icon(
-                                Icons.arrow_drop_down,
-                                color: Color(0xFF102C14),
-                                size: 35,
-                              )
-                            ],
-                          )
-                      )
-                  )
-              )
-            ]
-        );
+        return DiagramCard(name: sensorData[index].name);
       }
     );
   }
---
+++ lib/widgets/diagram_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class DiagramCard extends StatelessWidget {
+  const DiagramCard({
+    super.key,
+    required this.name
+  });
+
+  final String? name;
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Row(
+        children: [
+          Container(
+              width: 800,
+              height: 100,
+              padding: const EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                  color: Color(0xFF102C14),
+                  borderRadius: BorderRadius.circular(20.0)
+              ),
+              margin: EdgeInsets.symmetric(horizontal: width * 0.03, vertical: height * 0.05),
+              child: Card(
+                  color: Color(0xFFD4E4D7),
+                  child: Container(
+                      margin: EdgeInsets.only(left: 15, right: 15),
+                      child: Row(
+                        children: [
+                          Text(
+                              "${name}",
+                              style: TextStyle(
+                                  color: Color(0xFF102C14),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20
+                              )
+                          ),
+                          Spacer(),
+                          Icon(
+                            Icons.arrow_drop_down,
+                            color: Color(0xFF102C14),
+                            size: 35,
+                          )
+                        ],
+                      )
+                  )
+              )
+          )
+        ]
+    );
+  }
+}
