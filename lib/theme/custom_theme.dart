7d5f73a898265831f98c5590df5edacc69c11338
TatianaChF <tatiana.chefox@gmail.com>
2025-02-11 11:10:09 +0300

    removing unnecessary imports

--- lib/theme/custom_theme.dart
+++ lib/theme/custom_theme.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 ThemeData _buildCustomTheme() {
   const Color primaryColor = Color(0xFFD4E4D7);
   const Color secondaryColor = Color(0xFF102C14);
