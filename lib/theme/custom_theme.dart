ffe9f37e3391e2e0a436912a1094a9dca5dae300
TatianaChF <tatiana.chefox@gmail.com>
2025-02-11 10:30:45 +0300

    add test color sheme style

--- lib/theme/custom_theme.dart
+++ lib/theme/custom_theme.dart
@@ -6,5 +6,11 @@ ThemeData _buildCustomTheme() {
   const Color primaryColor = Color(0xFF102C14);
   const Color secondaryColor = Color(0xFFD4E4D7);
 
-  
+  return ThemeData(
+    colorScheme: ColorScheme.light(
+      primary: primaryColor,
+      secondary: secondaryColor,
+      surface: Colors.white
+    )
+  );
 }
