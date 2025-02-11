9330d494005e1745341f1f76c8748b2271809156
TatianaChF <tatiana.chefox@gmail.com>
2025-02-11 10:24:19 +0300

    add primaryColor and secondaryColor

---
+++ lib/theme/custom_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+ThemeData _buildCustomTheme() {
+  const Color primaryColor = Color(0xFF102C14);
+  const Color secondaryColor = Color(0xFFD4E4D7);
+
+  
+}
